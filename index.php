0cfaa4c9ce1a0f6114666100699c6e87322d005e
simonasofronici <120501805+simonasofronici@users.noreply.github.com>
2022-12-13 21:12:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+<body>
+    <form method="post" action="" enctype='multipart/form-data'>
+        <input type='file' name='file'>
+        <input type='submit' value='Upload' name='but_upload'>
+    </form>
+</body>
+</html>
+
+
+
+
